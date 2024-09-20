22f74234a8e70fcc4aee0a6f8fce097c01a6428a
sokcheapanha <sokcheapanha11@gmail.com>
2024-09-20 16:11:38 +0700

    update bean type

--- src/main/kotlin/com/panha/core_backend/core/service/impl/IGenericService.kt
+++ src/main/kotlin/com/panha/core_backend/core/service/impl/IGenericService.kt
@@ -6,22 +6,18 @@ import com.panha.core_backend.core.repo.BaseRepository
 import com.panha.core_backend.core.service.GenericService
 import com.panha.core_backend.utilities.UtilService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.lang.Nullable
-import org.springframework.stereotype.Service
 
-@Service
-class IGenericService<T : BaseEntity>: GenericService<T> {
-
-    @Nullable @Autowired lateinit var repository: BaseRepository<T>
+abstract class IGenericService<T : BaseEntity>: GenericService<T> {
+    @Autowired lateinit var repository: BaseRepository<T>
     @Autowired lateinit var utilService: UtilService
-
     override fun save(entity: T): T {
         return repository.save(entity)
     }
 
     override fun update(entity: T, id: Long): T {
         val existingEntity = this.findById(id)
-        val updatedEntity = utilService.bindProperties(entity , existingEntity , exclude = listOf("id" , "created" , "createdBy"))
+        val updatedEntity =
+            utilService.bindProperties(entity, existingEntity, exclude = listOf("id", "created", "createdBy"))
         repository.save(updatedEntity as T)
         return existingEntity
     }
@@ -39,10 +35,10 @@ class IGenericService<T : BaseEntity>: GenericService<T> {
     }
 
     private fun findById(id: Long): T {
-        return repository.findById(id).orElseThrow { NotFoundExceptionCustom("${this.entityClass.name} with id $id not found") }
+        return repository.findById(id)
+            .orElseThrow { NotFoundExceptionCustom("${this.entityClass.name} with id $id not found") }
     }
 
-    // This helper allows retrieving the class name of T
     private val entityClass: Class<T>
-        get() = repository.javaClass.genericSuperclass as Class<T>
+        get() = repository::class.java.simpleName as Class<T>
 }
